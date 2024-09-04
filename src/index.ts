dee64ceb4b1068ace9a9f9100dea13bd66b321a5
FarrazID2 <farraz02@gmail.com>
2024-09-04 11:35:14 +0700

    Fix bugs for release 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,24 @@ const port = 3000;
 app.use(express.json());
 
 // Endpoint untuk register user
+//TODO: Test route-1: POST http://localhost:3000/register
 app.post('/register', (req: Request, res: Response) => {
   const { username, password } = req.body;
   // Logika untuk register user
-  res.send('User registered');
+  // res.send('User registered');
+  res.status(201).json({
+    message: 'User registered',
+    data: {
+      username,
+      password,
+    },
+  });
+
+  console.log('Test of Error =>');
 });
 
 // Endpoint untuk login user
+//TODO: Test route-2: POST http://localhost:3000/login
 app.post('/login', (req: Request, res: Response) => {
   const { username, password } = req.body;
   // Logika untuk login user
@@ -33,6 +44,7 @@ app.post('/products', (req: Request, res: Response) => {
 });
 
 // Endpoint untuk mendapatkan semua produk
+//TODO: Test route-3: GET http://localhost:3000/products
 app.get('/products', (req: Request, res: Response) => {
   // Logika untuk mendapatkan semua produk
   res.send('List of products');
