73ff143e07054999429bbd058c704d87451723d6
FarrazID2 <farraz02@gmail.com>
2024-09-04 10:56:30 +0700

    Add order processing endpoints

--- src/index.ts
+++ src/index.ts
@@ -37,3 +37,17 @@ app.get('/products', (req: Request, res: Response) => {
   // Logika untuk mendapatkan semua produk
   res.send('List of products');
 });
+
+//TODO: update commit > "Add order processing endpoints"
+// Endpoint untuk membuat order
+app.post('/orders', (req: Request, res: Response) => {
+  const { productId, quantity } = req.body;
+  // Logika untuk membuat order
+  res.send('Order created');
+});
+
+// Endpoint untuk mendapatkan semua order
+app.get('/orders', (req: Request, res: Response) => {
+  // Logika untuk mendapatkan semua order
+  res.send('List of orders');
+});
